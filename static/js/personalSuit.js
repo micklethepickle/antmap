9b4ea40502990f12157137d34ef4bef3f7148558
fanbeatsman <fan_ma8@hotmail.com>
2014-08-21 21:33:03 -0400

    finally! geocode works in my js

---
+++ static/js/personalSuit.js
@@ -0,0 +1,40 @@
+
+
+function makeGeocodeRequest(query){
+
+var one = 1;
+var request = "QEWQWEqw" + "QWEQWEWQe";
+var request = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query.replace(/ /g, "+") + "&key=AIzaSyBxcLsh5UEYOPRUW_q3uCwLhcVTcYiXfaY";
+console.log(request);
+return request;
+}
+
+
+
+
+function httpGet(URL){
+
+var xmlHttp = null;
+xmlHttp = new XMLHttpRequest();
+xmlHttp.open("GET", URL , false);
+xmlHttp.send(null);
+
+return JSON.parse(xmlHttp.response);
+
+}
+
+
+function geocode(address, latOrLong){
+
+var jsonResponse = httpGet(makeGeocodeRequest(address));
+
+if (latOrLong.toLowerCase() == "lat"){
+console.log (jsonResponse.results[0].geometry.location.lat);
+return parseFloat(jsonResponse.results[0].geometry.location.lat);
+}
+else if (latOrLong.toLowerCase() == "long"){
+console.log(parseFloat(jsonResponse.results[0].geometry.location.lng))
+return parseFloat(jsonResponse.results[0].geometry.location.lng);
+}
+
+}
