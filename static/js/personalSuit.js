dd2f8594f53c2d56a0950830d20a12db6d7f3079
fanbeatsman <fan_ma8@hotmail.com>
2014-08-28 23:07:12 -0400

    forgot to commit

--- static/js/personalSuit.js
+++ static/js/personalSuit.js
@@ -1,12 +1,10 @@
 
-
 function makeGeocodeRequest(query){
 
-var one = 1;
-var request = "QEWQWEqw" + "QWEQWEWQe";
-var request = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query.replace(/ /g, "+") + "&key=AIzaSyBxcLsh5UEYOPRUW_q3uCwLhcVTcYiXfaY";
-console.log(request);
-return request;
+	var one = 1;
+	var request = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query.replace(/ /g, "+") + "&key=AIzaSyBxcLsh5UEYOPRUW_q3uCwLhcVTcYiXfaY";
+	console.log(request);
+	return request;
 }
 
 
@@ -14,27 +12,41 @@ return request;
 
 function httpGet(URL){
 
-var xmlHttp = null;
-xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", URL , false);
-xmlHttp.send(null);
+	var xmlHttp = null;
+	xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", URL , false);
+	xmlHttp.send(null);
 
-return JSON.parse(xmlHttp.response);
+	return JSON.parse(xmlHttp.response);
 
 }
 
 
 function geocode(address, latOrLong){
 
-var jsonResponse = httpGet(makeGeocodeRequest(address));
+	var jsonResponse = httpGet(makeGeocodeRequest(address));
 
-if (latOrLong.toLowerCase() == "lat"){
-console.log (jsonResponse.results[0].geometry.location.lat);
-return parseFloat(jsonResponse.results[0].geometry.location.lat);
-}
-else if (latOrLong.toLowerCase() == "long"){
-console.log(parseFloat(jsonResponse.results[0].geometry.location.lng))
-return parseFloat(jsonResponse.results[0].geometry.location.lng);
-}
+	if (latOrLong.toLowerCase() == "lat"){
+		console.log (jsonResponse.results[0].geometry.location.lat);
+		return parseFloat(jsonResponse.results[0].geometry.location.lat);
+	}
+	else if (latOrLong.toLowerCase() == "long"){
+		console.log(parseFloat(jsonResponse.results[0].geometry.location.lng))
+		return parseFloat(jsonResponse.results[0].geometry.location.lng);
+	}
 
 }
+
+
+
+/Some jquery/
+
+		$(document).ready(function(){
+			$("button").click(function(){
+				$("#map-canvas").toggle("fast");
+			}, function(){$(".span5").toggle("fast");
+			});
+		});
+
+
+
